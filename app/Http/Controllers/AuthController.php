09669397f47ad91053d06cdfb1e5f1247408ba52
hanif05 <muamarhanif05@gmail.com>
2019-07-25 11:13:35 +0700

    make method validation and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function index()
     {
         return view('pages.login.index');
+    }
+
+    public function validation()
+    {
+        if (Auth::attempt(request()->only('email', 'password'))){
+            return redirect('/home');
+        }
+
+        return redirect('/');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect('/');
     }
 }
