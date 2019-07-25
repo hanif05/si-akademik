b5913e3e68ead959b8ad526b270f7d9f4503864f
hanif05 <muamarhanif05@gmail.com>
2019-07-25 16:12:27 +0700

    created list table guru

--- resources/views/pages/guru/index.blade.php
+++ resources/views/pages/guru/index.blade.php
@@ -3,5 +3,44 @@
 
 @section('content')
 
+<div class="container-fluid">
+	<div class="row page-titles">
+		<div class="col-md-5 align-self-center">
+			<h4 class="text-themecolor">Datatable</h4>
+		</div>
+		<div class="col-md-7 align-self-center text-right">
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Data Guru</h4>
+                                <h6 class="card-subtitle">Data semua guru</h6>
+                                <div class="table-responsive m-t-40">
+                                    <table id="myTable" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+		</div>
+	</div>
+</div>
+
+
 
 @endsection
