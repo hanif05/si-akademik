9b99ceb493e4e727cd622b4cbf1372f7847ba4a8
hanif05 <muamarhanif05@gmail.com>
2019-07-26 09:53:41 +0700

    fixing tag from

--- resources/views/pages/guru/index.blade.php
+++ resources/views/pages/guru/index.blade.php
@@ -56,59 +56,60 @@
 				<h4 class="modal-title">Tambah Data Guru</h4>
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 			</div>
-			<div class="modal-body">
-				<form action="{{ route('guru.store') }}" method="POST">
-					@csrf
-					<div class="form-group">
-						<label for="nip" class="control-label">Nip:</label>
-						<input type="text" class="form-control" id="nip" name="nip">
-					</div>
-					<div class="form-group">
-						<label for="nama" class="control-label">Nama:</label>
-						<input type="text" class="form-control" id="nama" name="nama">
-					</div>
-					<div class="form-group">
-						<label class="control-label">Jenis Kelamin:</label>
-						<select class="form-control">
-							<option value="Laki-laki">Laki-laki</option>
-							<option value="Perempuan">Perempuan</option>
-						</select>
-						<small class="form-control-feedback"> Select your gender </small> 
-					</div>
-					<div class="form-group">
-						<label for="tmpt_lahir" class="control-label">Tempat Lahir:</label>
-						<input type="text" class="form-control" id="tmpt_lahir" name="tmpt_lahir">
-					</div>
-					<div class="form-group">
-						<label for="tgl_lahir" class="control-label">Tanggal Lahir:</label>
-						<input type="text" class="form-control" id="tgl_lahir" name="tgl_lahir">
-					</div>
-					<div class="form-group">
-						<label for="telp" class="control-label">Telepon:</label>
-						<input type="text" class="form-control" id="telp" name="telp">
-					</div>
-					<div class="form-group">
-						<label for="alamat" class="control-label">Alamat:</label>
-						<textarea class="form-control" id="alamat" name="alamat"></textarea>
-					</div>
-					<div class="form-group">
-						<label class="control-label">Upload Foto:</label>
-						<div class="input-group">
-							<div class="input-group-prepend">
-								<span class="input-group-text">Upload</span>
-							</div>
-							<div class="custom-file">
-								<input type="file" class="custom-file-input" id="foto" name="foto">
-								<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+			<form action="{{ route('guru.store') }}" method="POST" enctype="multipart/form-data">
+				<div class="modal-body">
+						@csrf
+						<div class="form-group">
+							<label for="nip" class="control-label">Nip:</label>
+							<input type="text" class="form-control" id="nip" name="nip">
+						</div>
+						<div class="form-group">
+							<label for="nama" class="control-label">Nama:</label>
+							<input type="text" class="form-control" id="nama" name="nama">
+						</div>
+						<div class="form-group">
+							<label class="control-label">Jenis Kelamin:</label>
+							<select class="form-control" name="jk">
+								<option value="Laki-laki">Laki-laki</option>
+								<option value="Perempuan">Perempuan</option>
+							</select>
+							<small class="form-control-feedback"> Select your gender </small> 
+						</div>
+						<div class="form-group">
+							<label for="tmpt_lahir" class="control-label">Tempat Lahir:</label>
+							<input type="text" class="form-control" id="tmpt_lahir" name="tmpt_lahir">
+						</div>
+						<div class="form-group">
+							<label class="control-label">Tanggal Lahir:</label>
+							<input type="date" class="form-control" id="tgl_lahir" name="tgl_lahir">
+						</div>
+						<div class="form-group">
+							<label for="telp" class="control-label">Telepon:</label>
+							<input type="text" class="form-control" id="telp" name="telp">
+						</div>
+						<div class="form-group">
+							<label for="alamat" class="control-label">Alamat:</label>
+							<textarea class="form-control" id="alamat" name="alamat"></textarea>
+						</div>
+						<div class="form-group">
+							<label class="control-label">Upload Foto:</label>
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text">Upload</span>
+								</div>
+								<div class="custom-file">
+									<input type="file" class="custom-file-input" id="foto" name="foto">
+									<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+								</div>
 							</div>
 						</div>
-					</div>
-				</form>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-danger waves-effect waves-light">Save changes</button>
-			</div>
+					
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-danger waves-effect waves-light">Save</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
