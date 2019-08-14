7ec837cff12029a1c8f103c6f9eebad8317a1b85
hanif05 <muamarhanif05@gmail.com>
2019-08-14 13:49:32 +0700

    added file _modal

---
+++ resources/views/layouts/includes/_modal.blade.php
@@ -0,0 +1,22 @@
+<!-- Modal -->
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header" id="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title" id="modal-title" title=""></h4>
+
+			</div>
+			<div class="modal-body" id="modal-body">
+
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary" id="modal-btn-save">Simpan</button>
+			</div>
+
+		</div>
+	</div>
+</div>
