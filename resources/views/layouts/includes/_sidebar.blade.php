b6e4de1519b75147389c48fc3b3bf4ad85388384
hanif05 <muamarhanif05@gmail.com>
2019-08-14 16:12:06 +0700

    fixed layout sidebar

--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -4,7 +4,7 @@
                 <div class="user-pro-body">
                     <div><img src="{{ asset('asset') }}/assets/images/users/2.jpg" alt="user-img" class="img-circle"></div>
                     <div class="dropdown">
-                        <a href="javascript:void(0)" class="dropdown-toggle u-dropdown link hide-menu" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Steave Gection <span class="caret"></span></a>
+                        <a href="javascript:void(0)" class="dropdown-toggle u-dropdown link hide-menu" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ auth()->user()->name }} | {{ auth()->user()->level->name }}<span class="caret"></span></a>
                         <div class="dropdown-menu animated flipInY">
                             <!-- text-->
                             <a href="javascript:void(0)" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
