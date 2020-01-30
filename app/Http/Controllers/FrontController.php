6f7fc6aba5371dbf665e2b56b5fda953ffc54fe0
hanif05 <muamarhanif05@gmail.com>
2020-01-31 00:38:23 +0700

    repair frontController

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Berita;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index()
     {
-        return view('pages.front.index');
+        $post = Berita::orderBy('created_at', 'DESC')->take(2)->get();
+        
+        return view('pages.front.index', compact('post'));
     }
 }
