1828bf2e05fb6d21bdb414a90d0a2b3e4d69d731
hanif05 <muamarhanif05@gmail.com>
2020-01-31 18:43:33 +0700

    repair comment

--- resources/views/pages/front/single-blog.blade.php
+++ resources/views/pages/front/single-blog.blade.php
@@ -133,7 +133,7 @@
                     </div>
                 </div>
                 <div class="comments-area">
-                    <h4>05 Comments</h4>
+                    <h4>{{ $countComment }} Comments</h4>
                     @if ($post->comments)
                         @foreach ($post->comments as $item)
                             <div class="comment-list">
@@ -146,7 +146,7 @@
                                             @if ($item->user())
                                             <h5><a href="#">{{ $item->user->name }}</a></h5>
                                             @endif
-                                            <p class="date">{{ $item->created_at->format('F d, Y') }} at {{ $item->created_at->format('h:m a') }} </p>
+                                            <p class="date">{{ $item->created_at->format('F d, Y') }} at {{ $item->created_at->format('h:i a') }} </p>
                                             <p class="comment">
                                                 {{ $item->content }}
                                             </p>
@@ -159,105 +159,15 @@
                             </div>
                         @endforeach
                     @endif
-                    <div class="comment-list left-padding">
-                        <div class="single-comment justify-content-between d-flex">
-                            <div class="user justify-content-between d-flex">
-                                <div class="thumb">
-                                    <img src="img/blog/c2.jpg" alt="">
-                                </div>
-                                <div class="desc">
-                                    <h5><a href="#">Elsie Cunningham</a></h5>
-                                    <p class="date">December 4, 2017 at 3:12 pm </p>
-                                    <p class="comment">
-                                        Never say goodbye till the end comes!
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="reply-btn">
-                                <a href="" class="btn-reply text-uppercase">reply</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="comment-list left-padding">
-                        <div class="single-comment justify-content-between d-flex">
-                            <div class="user justify-content-between d-flex">
-                                <div class="thumb">
-                                    <img src="img/blog/c3.jpg" alt="">
-                                </div>
-                                <div class="desc">
-                                    <h5><a href="#">Annie Stephens</a></h5>
-                                    <p class="date">December 4, 2017 at 3:12 pm </p>
-                                    <p class="comment">
-                                        Never say goodbye till the end comes!
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="reply-btn">
-                                <a href="" class="btn-reply text-uppercase">reply</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="comment-list">
-                        <div class="single-comment justify-content-between d-flex">
-                            <div class="user justify-content-between d-flex">
-                                <div class="thumb">
-                                    <img src="img/blog/c4.jpg" alt="">
-                                </div>
-                                <div class="desc">
-                                    <h5><a href="#">Maria Luna</a></h5>
-                                    <p class="date">December 4, 2017 at 3:12 pm </p>
-                                    <p class="comment">
-                                        Never say goodbye till the end comes!
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="reply-btn">
-                                <a href="" class="btn-reply text-uppercase">reply</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="comment-list">
-                        <div class="single-comment justify-content-between d-flex">
-                            <div class="user justify-content-between d-flex">
-                                <div class="thumb">
-                                    <img src="img/blog/c5.jpg" alt="">
-                                </div>
-                                <div class="desc">
-                                    <h5><a href="#">Ina Hayes</a></h5>
-                                    <p class="date">December 4, 2017 at 3:12 pm </p>
-                                    <p class="comment">
-                                        Never say goodbye till the end comes!
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="reply-btn">
-                                <a href="" class="btn-reply text-uppercase">reply</a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <div class="comment-form">
                     <h4>Leave a Reply</h4>
                     <form action="{{ route('comments.store') }}" method="POST">
                         @csrf
                         <input type="hidden" name="berita_id" value="{{ $post->id }}">
-                        <div class="form-group form-inline">
-                            <div class="form-group col-lg-6 col-md-6 name">
-                                <input type="text" class="form-control" id="name" placeholder="Enter Name" onfocus="this.placeholder = ''"
-                                    onblur="this.placeholder = 'Enter Name'">
-                            </div>
-                            <div class="form-group col-lg-6 col-md-6 email">
-                                <input type="email" class="form-control" id="email" placeholder="Enter email address"
-                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'">
-                            </div>
-                        </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="subject" placeholder="Subject" onfocus="this.placeholder = ''"
-                                onblur="this.placeholder = 'Subject'">
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control mb-10" rows="5" placeholder="Messege"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" name="content" required=""></textarea>
+                            <textarea class="form-control mb-10" rows="5" placeholder="Your Comment"
+                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Comment'" name="content" required=""></textarea>
                         </div>
                         <button class="primary-btn" type="submit">Post Comment</button>
                     </form>
